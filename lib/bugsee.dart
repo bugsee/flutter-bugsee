d564996e181ed185270f382148c80a59801a5bd6
Dmitry Fink <dmitry@finik.net>
2018-04-11 10:26:13 -0700

    Fixed formatting and dependecies
    Change-Id: Iadc17f5ace9a084ce834e9647e5d7804ad28e2b3

--- lib/bugsee.dart
+++ lib/bugsee.dart
@@ -8,24 +8,26 @@ import 'package:crypto/crypto.dart';
 import 'package:crypto/src/digest_sink.dart';
 
 class Bugsee {
-  static const MethodChannel _channel =
-      const MethodChannel('bugsee');
+  static const MethodChannel _channel = const MethodChannel('bugsee');
 
-  static Future<Null> event({@required String name, Map<String, dynamic> parameters}) async {
+  static Future<Null> event(
+      {@required String name, Map<String, dynamic> parameters}) async {
     await _channel.invokeMethod('event', <String, dynamic>{
       'name': name,
       'parameters': parameters,
     });
   }
 
-  static Future<Null> trace({@required String name, @required dynamic value}) async {
+  static Future<Null> trace(
+      {@required String name, @required dynamic value}) async {
     await _channel.invokeMethod('trace', <String, dynamic>{
       'name': name,
       'value': value,
     });
   }
 
-  static Future<Null> setAttribute({@required String key, @required dynamic value}) async {
+  static Future<Null> setAttribute(
+      {@required String key, @required dynamic value}) async {
     await _channel.invokeMethod('setAttribute', <String, dynamic>{
       'key': key,
       'value': value,
@@ -38,54 +40,58 @@ class Bugsee {
     });
   }
 
-  static Future<Null> logException({@required dynamic exception, @required bool handled, dynamic stackTrace}) async {
+  static Future<Null> logException(
+      {@required dynamic exception,
+      @required bool handled,
+      dynamic stackTrace}) async {
     final Chain chain = stackTrace is StackTrace
-      ? new Chain.forTrace(stackTrace)
-      : new Chain.parse(stackTrace);
+        ? new Chain.forTrace(stackTrace)
+        : new Chain.parse(stackTrace);
 
     final List<Map<String, dynamic>> frames = <Map<String, dynamic>>[];
 
-	  var ds = new DigestSink();
-	  var s = sha1.startChunkedConversion(ds);
-  	for (int t = 0; t < chain.traces.length; t += 1) {
-    	for (int f = 0; f < chain.traces[t].frames.length; f += 1) {
-    	  dynamic frame = chain.traces[t].frames[f];
+    var ds = new DigestSink();
+    var s = sha1.startChunkedConversion(ds);
+    for (int t = 0; t < chain.traces.length; t += 1) {
+      for (int f = 0; f < chain.traces[t].frames.length; f += 1) {
+        dynamic frame = chain.traces[t].frames[f];
 
-    	  dynamic uri = frame.uri;
-    	  dynamic user = true;
-    	  dynamic package = frame.package;
+        dynamic uri = frame.uri;
+        dynamic user = true;
+        dynamic package = frame.package;
 
         if (frame.uri.scheme != 'dart' && frame.uri.scheme != 'package')
           uri = frame.uri.pathSegments.last;
 
         if (frame.uri.scheme == 'hooks' ||
             frame.uri.scheme == 'dart' ||
-            frame.package == 'flutter')
-          user = false;
+            frame.package == 'flutter') user = false;
 
         if (package == null) {
-          if (frame.uri.scheme == 'dart') package = 'dart';
-          else package = 'application';
+          if (frame.uri.scheme == 'dart')
+            package = 'dart';
+          else
+            package = 'application';
         }
 
-    		frames.add(<String, dynamic>{
-			    'trace': '${frame.member} ($uri:${frame.line})',
+        frames.add(<String, dynamic>{
+          'trace': '${frame.member} ($uri:${frame.line})',
           'module': '$package',
-			    'user': user
-			  });
+          'user': user
+        });
 
-			  s.add(utf8.encode(frame.location));
-    	}
+        s.add(utf8.encode(frame.location));
+      }
 
       if (t < chain.traces.length - 1) {
-    	  frames.add(<String, dynamic>{
+        frames.add(<String, dynamic>{
           'trace': '<asyncronous gap>',
           'module': 'dart',
           'user': false,
         });
       }
-  	}
-  	s.close();
+    }
+    s.close();
 
     final dynamic ex = <String, dynamic>{
       'name': '${exception.runtimeType}',
